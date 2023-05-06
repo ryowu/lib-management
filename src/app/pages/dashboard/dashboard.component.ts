3042792fa2fe6ec0f567c5337d58d0f404d6f3d5
ryowu <ryowu@msn.com>
2023-05-06 17:54:57 +1000

    test database connection

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  public content: any;
+  public ngOnInit(): void {
+    fetch('https://lib-mgr-db.vercel.app/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: `query MyQuery {
+			allMyFirstTables {
+			  nodes {
+				id
+				name
+			  }
+			}
+		  }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => this.content = data);
+  }
 }
