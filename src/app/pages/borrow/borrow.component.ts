f04e7fc604655966ea64b29bd735d25aea2c05e1
ryowu <ryowu@msn.com>
2023-05-08 22:44:14 +1000

    - regular update

--- src/app/pages/borrow/borrow.component.ts
+++ src/app/pages/borrow/borrow.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-borrow',
-  templateUrl: './borrow.component.html',
-  styleUrls: ['./borrow.component.scss']
+	selector: 'app-borrow',
+	templateUrl: './borrow.component.html',
+	styleUrls: ['./borrow.component.scss'],
 })
 export class BorrowComponent {
-
+	public userName = '';
+	public applyUserName(){
+		this.userName = 'Tom Matthrew';
+	}
 }
